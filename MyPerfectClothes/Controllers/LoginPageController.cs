8977f1fae46ad01ef4aa61d80e1da80be6a547da
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-04-16 14:48:36 +0200

    Separate html code to diffrents views

---
+++ MyPerfectClothes/Controllers/LoginPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPerfectClothes.Controllers
+{
+    public class LoginPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
