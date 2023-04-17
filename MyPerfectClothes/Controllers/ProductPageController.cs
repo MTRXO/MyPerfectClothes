6b478e930a7852d4ce3ba62063871f0d7a6b7443
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-04-17 21:23:44 +0200

    create product page cotroller

---
+++ MyPerfectClothes/Controllers/ProductPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPerfectClothes.Controllers
+{
+    public class ProductPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
