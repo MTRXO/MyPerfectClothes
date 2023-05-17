8c2e86f83b88433c23c9ba16aa103c0d753bd745
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-05-17 14:34:55 +0200

    create product model

---
+++ MyPerfectClothes/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPerfectClothes.Models
+{
+    public class Class
+    {
+        public string  ClothesType { get; set; }
+        public double Price { get; set; }
+        public int Size { get; set; }
+        public string Color { get; set; }
+
+    }
+}
