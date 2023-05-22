3269e63aa7dff0b73a9bdccbd41c481af2778815
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-05-22 23:21:13 +0200

    add a cart controller and view

---
+++ MyPerfectClothes/Controllers/CartPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPerfectClothes.Controllers
+{
+    public class CartPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
