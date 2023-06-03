6444095147806ae9af22f3500783148122373721
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-06-03 15:27:34 +0200

    All orders controller takes a data from databas

--- MyPerfectClothes/Controllers/AllOrdersController.cs
+++ MyPerfectClothes/Controllers/AllOrdersController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPerfectClothes.Data;
 
 namespace MyPerfectClothes.Controllers
 {
     public class AllOrdersController : Controller
     {
+        private readonly AppDbContext _db;
+        public AllOrdersController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _db.OrderProp.ToList();
             return View();
         }
+
     }
 }
