08122f8f7fdad6b49ed2665e584da77222f351ed
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-06-03 15:00:25 +0200

    Create All order tab controller and View

---
+++ MyPerfectClothes/Controllers/AllOrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPerfectClothes.Controllers
+{
+    public class AllOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
