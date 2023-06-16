4b57176f4e6e85a5e21826cc77ad1d5d49923fed
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-06-17 01:23:12 +0200

    make controller display curent cart state

--- MyPerfectClothes/Controllers/CartPageController.cs
+++ MyPerfectClothes/Controllers/CartPageController.cs
@@ -7,10 +7,15 @@ namespace MyPerfectClothes.Controllers
 {
     public class CartPageController : Controller
     {
-
+        CurrentCartState CartRn = new CurrentCartState();
+       //public bool IsClicked = false;
         public IActionResult Index()
         {
-
+            CartRn.Name = "Shoes";
+            CartRn.Price = 20;
+            ViewBag.NameOfProduct = CartRn.Name;
+            ViewBag.PriceOfProduct = CartRn.Price;
+            //IsClicked = true;
             return View();
          
         }
